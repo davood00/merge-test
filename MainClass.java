57bb7c640e633806e39d6cf55512647a7f08c139
Davood Mazinanian <dmazinanian@gmail.com>
2019-01-13 14:46:18 -0800

    branch 3 - commit 2

--- MainClass.java
+++ MainClass.java
@@ -5,7 +5,7 @@ class MainClass {
     public static void main(String[] args) {
         System.err.println("Error");
         for (int j = 0; j < 10; j++) {
-            System.out.println(j);
+            System.out.println(j + 4);
         }
     }
 }
