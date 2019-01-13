c607849f3101afc59d1826fae9fc1ad19606c6e1
Davood Mazinanian <dmazinanian@gmail.com>
2019-01-13 12:54:13 -0800

    branch 3 - commit 1

--- MainClass.java
+++ MainClass.java
@@ -4,8 +4,8 @@ import java.awt.*;
 class MainClass {
     public static void main(String[] args) {
         System.err.println("Error");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+        for (int j = 0; j < 10; j++) {
+            System.out.println(j);
         }
     }
 }
